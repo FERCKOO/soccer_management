679378d12e65514fd26db1907548a1c50c475410
FernandoVazquz <a329568@uach.mx>
2022-11-04 11:55:32 -0600

    N

--- lib/Screens/calendar.dart
+++ lib/Screens/calendar.dart
@@ -1,5 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:filter_list/filter_list.dart';
+import 'package:horizontal_calendar/horizontal_calendar.dart';
 
 class CalendarPage extends StatelessWidget {
   static String id = 'Calendar_Page';
@@ -40,7 +42,7 @@ class CalendarPage extends StatelessWidget {
               ),
             ),
             /**
-           * Nombre del jugador
+           * Nombre de la liga
            */
             Container(
               child: Text(
@@ -68,7 +70,169 @@ class CalendarPage extends StatelessWidget {
                 ),
               ),
             ),
-            Text('data')
+            // Caja para el filtro
+            SizedBox(
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  Column(children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(left: 10),
+                          // Filtro
+                          child: InkWell(
+                            onTap: () {},
+                            // Contenedor del filtro
+                            child: Container(
+                              height: 25,
+                              color: Color(0xFF011C53),
+                              margin: EdgeInsets.only(
+                                top: 5,
+                                bottom: 5,
+                              ),
+                              // Contenedor del nombre
+                              child: Row(children: [
+                                Container(
+                                  padding: EdgeInsets.only(
+                                    left: 15,
+                                  ),
+                                  child: Text(
+                                    'Liga',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 13,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                // Contenedor de la flecha para abajo
+                                Container(
+                                    padding:
+                                        EdgeInsets.only(left: 5.0, top: 2.0),
+                                    child: Icon(
+                                      Icons.arrow_drop_down,
+                                      size: 17,
+                                      color: Colors.white,
+                                    )),
+                              ]),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ]),
+                ],
+              ),
+            ),
+            // Contenedor del mes
+            Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 5,
+              ),
+              margin: EdgeInsets.only(
+                top: 5,
+                bottom: 5,
+              ),
+              child: Row(children: [
+                //Contenedor del mes
+                Container(
+                  padding: EdgeInsets.only(left: 5.0),
+                  child: Text(
+                    'Nombre mes',
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'lato'),
+                  ),
+                ),
+              ]),
+            ),
+            HorizontalCalendar(
+              date: DateTime.now(),
+              textColor: Colors.black45,
+              backgroundColor: Colors.white,
+              selectedColor: Colors.blue,
+              onDateSelected: (date) => print(
+                date.toString(),
+              ),
+            ),
+            Text(
+              'Upcoming',
+              style: TextStyle(
+                color: Color(0xFF011C53),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Lato',
+              ),
+            ),
+
+            // Carta para los partidos
+            Container(
+              child: Card(
+                elevation: 10,
+                color: Colors.white,
+                child: SizedBox(
+                  width: 300,
+                  height: 150,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'GeeksforGeeks',
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Lato'), //Textstyle
+                        ),
+                        Container(
+                          child: Row(
+                            children: [
+                              Icon(Icons.location_on_outlined),
+                              Text('Ubicacion del partido')
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Row(
+                            children: [
+                              Icon(Icons.av_timer),
+                              Text('Hora del partido')
+                            ],
+                          ),
+                        ),
+                        /**
+                         * Linea horizontal.
+                        */
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(
+                                color: Colors.grey,
+                                width: 1.5,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text(
+                          'k',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                          ), //Textstyle
+                        ), //Text
+                        const SizedBox(
+                          height: 10,
+                        ), //SizedBox//SizedBox
+                      ],
+                    ), //Column
+                  ), //Padding
+                ), //SizedBox
+              ),
+            ),
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'Screens/calendar.dart';
+import 'Screens/principal.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       // Pantalla de inicio
       initialRoute: CalendarPage.id,
       routes: {
-        CalendarPage.id:(context) => CalendarPage(),
+        CalendarPage.id: (context) => CalendarPage(),
       },
     );
   }
